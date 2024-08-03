c11611a025c8abd1f7b761bb62531447020334a8
bigahk <nic.medina0424@gmail.com>
2024-08-03 14:07:33 -0400

    Modify initial JS to be contained in functions with squares and userInput as global variables
    Add callPrompt() to be called when "Change Grid size" button is clicked
    Add if statement to check for user input validity (ensures number is being inputed)
    Store user input in squares global variable
    Call createBoxes() function within callPrompt() function to clear previous grid and create new grid based on new value of squares variable
    Add "Clear Grid" button to format existing grid without changing grid size
    "Clear All" button calls clearAll() function: changes background color to white, erasing earlier drawings/input
    
    Add parent div for buttons so that they can be displayed despite body having flex-direction: column

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-const container = document.getElementById("container");
-
 let squares = 16;
-let containerWidth = getComputedStyle(container).width;
+let userInput;
 
-//gets parent width and uses that to calculate square width (width divided by squares)
-let squareSize = Math.floor(containerWidth.replace("px", "") / squares);
-console.log(squareSize);
 
-for (i = 0; i < squares * squares; i++) {
-    const box = document.createElement("div");
-    box.className = "box";
-    container.appendChild(box);
-    box.style.width = squareSize + "px";
-    box.addEventListener("mouseover", fillIn);
-    function fillIn() {
-        box.style.backgroundColor = "black";
+function createBoxes() {
+    const container = document.getElementById("container");
+    let containerWidth = getComputedStyle(container).width;
+    //gets parent width and uses that to calculate square width (width divided by squares)
+    let squareSize = Math.floor(containerWidth.replace("px", "") / squares);
+    for (i = 0; i < squares * squares; i++) {
+        const box = document.createElement("div");
+        box.className = "box";
+        container.appendChild(box);
+        box.style.width = squareSize + "px";
+        box.addEventListener("mouseover", fillIn);
+        function fillIn() {
+            box.style.backgroundColor = "black";
+        }
     }
 }
 
+function callPrompt() {
+    let question = prompt("You can choose how large the grid is! Enter a prompt between 1 and 100.")
+    const promptButton = document.getElementById("prompt");
+
+    if (question !== null && isNaN(question) === false) {
+        squares = question;
+        const deleteBoxes = document.querySelectorAll('.box');
+        deleteBoxes.forEach(box => {
+            box.remove();
+        });
+
+        createBoxes();
+    }
+}
+
+function clearAll() {
+   const boxes = document.getElementsByClassName("box");
+
+   for (let i = 0; i <boxes.length; i++) {
+    boxes[i].style.backgroundColor = "white";
+   }
+
+}
 
 
+createBoxes();
